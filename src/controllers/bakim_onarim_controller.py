4b5a07582b01b8f445101a04c76a88e373199549
Dogukan Uckan <dogukanuckan@gmail.com>
2025-03-18 12:48:00 +0300

    bakim_onarim insert hatası düzeltilir

--- src/controllers/bakim_onarim_controller.py
+++ src/controllers/bakim_onarim_controller.py
@@ -4,6 +4,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(),'..', 'models')))
 
 from models.database import DbConnector
+from models.entity.bakim_onarim import BakimOnarim
 
 class BakimOnarimRepository:
     def __init__(self):
@@ -11,44 +12,49 @@ class BakimOnarimRepository:
         self.conn = self.db.connect()
     
     """ Yeni Bakım Onarım ekleme"""
-    def insert_Bakim_Onarim(self,arac_id, aciklama, tarih, kilometre, tutar):
+    def insert_Bakim_Onarim(self, bakim_onarim: BakimOnarim):
         query = """
         INSERT INTO bakim_onarim (arac_id, aciklama, tarih, kilometre, tutar)
         VALUES (?, ?, ?, ?, ?)
         """
         cursor = self.conn.cursor()
-        cursor.execute(query,(arac_id, aciklama, tarih, kilometre, tutar))
+        cursor.execute(query,(bakim_onarim.arac_id, bakim_onarim.aciklama, bakim_onarim.tarih, bakim_onarim.kilometre, bakim_onarim.tutar))
         self.conn.commit()
-        print("Bakim Onarim Ekleme Işlemi basarili...")
+        bakim_onarim.id = cursor.lastrowid
+        print("Bakim Onarim Ekleme Işlemi basarili...",bakim_onarim)
         
     """Bakim Onarım Listeleme"""
     def get_All_Bakim_Onarim(self):
         query = "SELECT * FROM bakim_onarim"
         cursor = self.conn.cursor()
         cursor.execute(query)
-        return cursor.fetchall()
+        return [BakimOnarim.from_tuple(row) for row in cursor.fetchall()]
     
     """Arac id Ile Bakim Onarim Getirme"""
-    def get_Bakim_Onarim_By_AracId(self, arac_id):
+    def get_Bakim_Onarim_By_AracId(self, arac_id: int):
         query = "SELECT * FROM bakim_onarim WHERE arac_id = ?"
         cursor = self.conn.cursor()
         cursor.execute(query,(arac_id,))
-        return cursor.fetchall()
+        return BakimOnarim.from_tuple(cursor.fetchall())
     
     """Bakim Onarim Guncelleme"""
-    def update_Bakim_Onarim(self,bakim_id,arac_id, aciklama, tarih, kilometre, tutar):
+    def update_Bakim_Onarim(self,bakim_onarim : BakimOnarim):
+        
+        if bakim_onarim.id is None:
+            raise ValueError("Güncellenecek bakım onarımın ID'si olmalı.")
+       
         query = """
             UPDATE bakim_onarim SET arac_id = ?, aciklama = ?, tarih = ?, kilometre = ?, tutar = ?
             WHERE id = ?
         """
         cursor = self.conn.cursor()
-        cursor.execute(query,(arac_id, aciklama, tarih, kilometre, tutar, bakim_id))
+        cursor.execute(query,(bakim_onarim.arac_id, bakim_onarim.aciklama, bakim_onarim.tarih, bakim_onarim.kilometre, bakim_onarim.tutar, bakim_onarim.bakim_id))
         self.conn.commit()
-        print("Bakim Onarim Guncelleme Islemi Basarili...")
+        print("Bakim Onarim Guncelleme Islemi Basarili...",bakim_onarim)
         
         
     """Bakim Onarim Silme"""
-    def delete_Bakim_Onarim(self,bakim_id):
+    def delete_Bakim_Onarim(self, bakim_id : int):
         query = "DELETE FROM bakim_onarim WHERE id = ?"
         cursor = self.conn.cursor()
         cursor.execute(query, (bakim_id,))
--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..', 'models')))
 from controllers.arac_controller import AracRepository  # Doğru modül adı ile içe aktar
 from controllers.bakim_onarim_controller import BakimOnarimRepository
 from models.entity.arac import Arac
+from models.entity.bakim_onarim import BakimOnarim
 
 # arac_repository nesnesini oluştur
 arac_repo = AracRepository()
@@ -19,7 +20,7 @@ araba = Arac("20APP98", "2025", "Porsche", "911", "Yussy", "Siyah", 1)
 #arac_repo.insert_arac("34ABC123", "2022", "BMW", "320i", "Ahmet Yılmaz", "Mavi", 1)
 #arac_repo.insert_arac("06DEF456", "2020", "Mercedes", "C200", "Mehmet Kaya", "Siyah", 1)
 arac_repo.insert_arac(araba)
-
+"""
 # Tüm araçları getir ve yazdır
 araclar = arac_repo.get_all_araclar()
 print("Araçlar:", araclar)
@@ -41,24 +42,17 @@ arac_repo.delete_arac(2)
 # Kalan araçları yazdır
 araclar = arac_repo.get_all_araclar()
 print("Son Güncellenmiş Araçlar:", araclar)
-
+"""
 #Yeni Bakim Onarim Ekle 
-bakim_onarim_repo.insert_Bakim_Onarim(6, "Yag degisimi", "17.03.2025", "1234", "1250.40")
-bakim_onarim_repo.insert_Bakim_Onarim(5, "Kilometre Sifirlama", "14.03.2025", "200000", "200")
-
+bakim_onarim = BakimOnarim(None,31,"Yag Degisimi", "10.03.2025", "123", "250")
+bakim_onarim_repo.insert_Bakim_Onarim(bakim_onarim)
 #Tüm Bakim Onarimi Listeleme
-bakim_onarimlar = bakim_onarim_repo.get_All_Bakim_Onarim()
-print("Bakim-Onarimlar: ",bakim_onarimlar)
 
 #Arac id ile bakim-onarim getirme
-bakim = bakim_onarim_repo.get_Bakim_Onarim_By_AracId(5)
-print("Bakim Olmus mu: ",bakim)
 
 #Bakim Guncelleme
-guncel_bakim = bakim_onarim_repo.update_Bakim_Onarim(1, 6, "Yanlis yag", "18.03.2025", "1234", "0")
 
 #Bakim Silme
-bakim_onarim_repo.delete_Bakim_Onarim(1)
 
 # Bağlantıyı kapat
 arac_repo.close()
